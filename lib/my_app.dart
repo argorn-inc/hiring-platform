660bf74186d9e0737d8b8b88675263754a760309
ImmArkorful <e.arkorful3@gmail.com>
2022-05-09 00:06:42 +0000

    feat: profile screen

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hiring_platform/home_page.dart';
+import 'package:hiring_platform/profile.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Onboard(),
+      home: const ProfileScreen(),
     );
   }
 }
